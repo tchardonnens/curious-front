58a4ce2151948bed9b4ecd0fc10cb1344c38ca6c
Thomas Chardo <tchardonnens@juniorisep.com>
2023-04-17 14:14:29 +0200

    try to add sidebar

--- curious/src/components/header.tsx
+++ curious/src/components/header.tsx
@@ -1,14 +1,15 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function Header() {
   return (
     <header className="flex justify-between items-center w-full p-5 border-b-2 pb-7 sm:px-4 px-2">
-      <a href="/">
+      <Link href="/">
         <h1 className="sm:text-4xl text-2xl font-bold ml-2 tracking-tight">Curious ⚡️</h1>
-      </a>
+      </Link>
 
       <div className="flex items-center">
-        <a href="/search" className="mr-2">Search</a>
+        <Link href="/search" className="mr-2">Search</Link>
       </div>
     </header>
   )
--- curious/src/components/sidebar.tsx
+++ curious/src/components/sidebar.tsx
@@ -9,14 +9,14 @@ export default function Sidebar(props: SidebarPropsList) {
       <div className="narrow-sidebar hidden lg:block lg:flex-shrink-0 lg:bg-gray-800 lg:overflow-y-auto">
         <div className="relative w-60 flex space-y-16 flex-col p-3">
 
-          {props.history.map((item) => {
+          {/* {props.history.map((item) => {
             return (
               <div className="flex flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
                 <h1 className="text-2xl font-bold text-white">{item.header}</h1>
                 <p className="text-white">{item.description}</p>
               </div>
             );
-          })}
+          })} */}
 
 
         </div>
