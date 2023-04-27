13be1f6293c6a9b2e8d74fc7f9b6ee851cf2bb62
Bora <borabalos9@gmail.com>
2023-04-27 22:37:16 +0200

    sign-in/up responsive

--- curious/src/pages/signin.tsx
+++ curious/src/pages/signin.tsx
@@ -24,7 +24,7 @@ const Auth = () => {
           </Head>
           <Header />
           <div className='flex justify-center'>
-            <div className='bg-transparent px-16 py-16 self-center mt-2 lg:w-2/5 md:w-3/5 lg:max-w-2/5 w-full'>
+            <div className='bg-transparent px-8 md:px-16 py-16 self-center mt-2 lg:w-2/5 md:w-3/5 lg:max-w-2/5 w-full'>
                 <h2 className='text-black text-4xl mb-8 font-semibold'>
                     Sign in
                 </h2>
