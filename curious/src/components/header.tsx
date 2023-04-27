459fbbb096e8e9b7a684283e95aad37bb5d75d2f
Bora <borabalos9@gmail.com>
2023-04-27 17:54:49 +0200

    navbar animated sliding indicator

--- curious/src/components/header.tsx
+++ curious/src/components/header.tsx
@@ -1,35 +1,41 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function Header() {
   const router = useRouter();
   let headerContent;
-  let searchLinkClassName = ' font-semibold px-3 py-1 border-2 rounded-3xl duration-200 ease-in-out';
-  let feedLinkClassName = ' font-semibold px-3 py-1 border-2 rounded-3xl duration-200 ease-in-out';
+  const indicatorRef = useRef<HTMLDivElement | null>(null);
 
-  if (router.pathname === '/feed') {
-    feedLinkClassName += ' border-black text-black dark:border-darkGrey dark:text-darkGrey ';
-    searchLinkClassName += ' border-darkGrey text-darkGrey hover:border-darkerGrey hover:text-darkerGrey dark:border-darkerGrey dark:text-darkerGrey dark:hover:border-darkGrey hover:dark:text-darkGrey ';
-  } else if (router.pathname === '/search') {
-    searchLinkClassName += ' border-black text-black dark:border-darkGrey dark:text-darkGrey';
-    feedLinkClassName += ' border-darkGrey text-darkGrey hover:border-darkerGrey hover:text-darkerGrey dark:border-darkerGrey dark:text-darkerGrey dark:hover:border-darkGrey hover:dark:text-darkGrey ';
-  }
+  useEffect(() => {
+    const path = router.pathname;
+    if (indicatorRef.current) {
+      if (path === '/feed') {
+        indicatorRef.current.style.left = '0';
+        indicatorRef.current.style.transform = 'translateX(131.3333%)';
+      } else if (path === '/search') {
+        indicatorRef.current.style.right = '0';
+        indicatorRef.current.style.transform = 'translateX(-131.3333%)';
+      }
+    }
+  }, [router.pathname]);
 
   if (router.pathname === '/feed' || router.pathname === '/search') {
     headerContent = (
       <>
-        <div className='flex flex-row items-center gap-5'>
-          <div className="text-center">
-            <Link href="/search" className={searchLinkClassName}>Search</Link>
-          </div>
-          <div className="text-center">
-            <Link href="/feed" className={feedLinkClassName}>Feed</Link>
-          </div>
+        <div className='bg-lightGrey dark:bg-darkerGrey rounded-full p-2 flex flex-row items-center gap-5 relative'>
+          <Link href="/search" className='text-center w-16 relative z-10'>
+            Search
+          </Link>
+          <Link href="/feed" className='text-center w-16 relative z-10'>
+            Feed
+          </Link>
+          <div className="indicator absolute z-0 bottom-1/6 top-1/6 h-5/6 w-5/12 bg-white dark:bg-black rounded-full transform transition-all duration-300 ease-in-out" ref={indicatorRef}></div>
         </div>
-        <div className='h-10 w-10 border-2 border-black dark:border-darkGrey rounded-full overflow-hidden cursor-pointer'>
+        <Link href="/profile" className='h-10 w-10 border-2 border-black dark:border-darkGrey rounded-full overflow-hidden cursor-pointer'>
           <img className='w-full height-full' src="https://pyxis.nymag.com/v1/imgs/d6a/dc7/4a5001b7beea096457f480c8808572428b-09-roll-safe.rsquare.w700.jpg" alt="User" />
-        </div>
+        </Link>
       </>
     );
   } else if (router.pathname === '/signup' || router.pathname === '/signin') {
