ff4ba32ddd09c55592cbd25d5969084d322a024f
Bora <62065271+Borab2001@users.noreply.github.com>
2023-04-28 00:59:32 +0200

    ev -> e signup

--- curious/src/pages/signup.tsx
+++ curious/src/pages/signup.tsx
@@ -30,9 +30,9 @@ const Auth = () => {
                     Sign up
                 </h2>
                 <div className='flex flex-col gap-4'>
-                    <Input label="Username" onChange={(ev: any) => setName(ev.target.value)} id="name" value={name}/>
-                    <Input label="Email" onChange={(ev: any) => setEmail(ev.target.value)} id="email" type="email" value={email}/>
-                    <Input label="Password" onChange={(ev: any) => setPassword(ev.target.value)} id="password" type="password" value={password}/>
+                    <Input label="Username" onChange={(e: any) => setName(e.target.value)} id="name" value={name}/>
+                    <Input label="Email" onChange={(e: any) => setEmail(e.target.value)} id="email" type="email" value={email}/>
+                    <Input label="Password" onChange={(e: any) => setPassword(e.target.value)} id="password" type="password" value={password}/>
                 </div>
                 <button className='bg-black py-3 text-white rounded-md w-full mt-10 hover:bg-dark hover:opacity-90 transition'>Register</button>
                 <button className='bg-white py-2.5 text-black rounded-md w-full mt-4 hover:bg-lightGrey transition flex flex-row items-center justify-center gap-4 border-2'>
