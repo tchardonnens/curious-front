e7a660b09813a6daa3ec492543738c6ec1784614
LucasCvlPrr <lucas.chevalier08@gmail.com>
2023-05-19 13:21:00 +0200

    Profile Page design

--- curious/src/pages/profile.tsx
+++ curious/src/pages/profile.tsx
@@ -1,16 +1,26 @@
 import Header from "@/components/header";
-import ProfileSidebar from "@/components/profileSidebar";
+import Sidebar from "@/components/sidebar";
 import Head from 'next/head';
 import Image from 'next/image';
+import React from "react";
 
 interface ProfileProps {
+  name: string;
   username: string;
   bio: string;
   followers: number;
   following: number;
 }
 
-export default function Profile(props: ProfileProps) {
+const Profile: React.FC<ProfileProps> = ({
+
+  name,
+  username,
+  bio,
+  followers,
+  following
+
+}) => {
   return (
     <>
       <Head>
@@ -27,35 +37,70 @@ export default function Profile(props: ProfileProps) {
 
       <div className="flex flex-row w-full h-full">
 
-        <ProfileSidebar></ProfileSidebar>
+        <Sidebar history={[]}></Sidebar>
 
 
 
-        <main className="bg-white dark:bg-anthracite p-10 flex w-full flex-col items-center justify-center text-center overflow-hidden">
-          <div className="bg-white border rounded shadow p-6 w-full">
-            <div className="flex items-center mb-4">
-              <div className="w-16 h-16 rounded-full overflow-hidden">
+        <main className="bg-white dark:bg-anthracite p-10 flex w-full flex-col overflow-hidden">
+
+          <div className="bg-white">
+
+            <div className="flex items-center relative">
+
+              <div className="w-full h-40">
+                <Image src="sf-dream.png" alt="Avatar" className="w-full h-full object-cover rounded-t-xl" />
+              </div>
+
+              <div className="w-24 h-24 rounded-full overflow-hidden absolute bottom-0 left-6 translate-y-12">
                 <Image src="PP-test.png" alt="Avatar" className="w-full h-full object-cover" />
               </div>
+
             </div>
 
-            <div className="flex flex-col">
-              <div className="flex">
-                <p>@username</p>
-              </div>
-              <div className="flex">
-                <p className="text-gray-700">Bio</p>
-              </div>
-              <div className="flex flex-row">
-                <div className="flex-none w-40">
-                  <span className="text-gray-600 pr-4">Followers</span>
-                  <span className="font-bold">290</span>
+            <div className="flex flex-col space-y-4 w-1/2 mt-10 px-4">
+              <div className="flex gap-4 items-center pt-6">
+                <div className="font-semibold text-lg">
+                  Name{name}
                 </div>
-                <div className="flex-none w-40">
-                  <span className="text-gray-600 pr-4">Following</span>
-                  <span className="font-bold">312</span>
+
+                <div className="text-neutral-600">
+                  @username{username}
                 </div>
               </div>
+
+
+              <div className="text-gray-600">
+                Bio ...... ....... ........ ....... ........ .......... ........... ......... ............ ............ .......... ......... ........{bio}
+              </div>
+
+              <div className="flex flex-row space-x-20">
+
+                <div className="flex flex-row space-x-4">
+
+                  <div>
+                    <span className="text-gray-600">Followers</span>
+                  </div>
+
+                  <div>
+                    <span className="font-semibold">180{followers}</span>
+                  </div>
+
+                </div>
+
+                <div className="flex flex-row space-x-4">
+
+                  <div>
+                    <span className="text-gray-600">Following</span>
+                  </div>
+
+                  <div>
+                    <span className="font-semibold">200{following}</span>
+                  </div>
+
+                </div>
+
+              </div>
+
             </div>
 
           </div>
@@ -64,3 +109,5 @@ export default function Profile(props: ProfileProps) {
     </>
   )
 }
+
+export default Profile;
