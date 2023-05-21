180d675c4ce79e511785337e437bd96db3981da4
Bora <borabalos9@gmail.com>
2023-05-22 00:18:43 +0200

    Fixed keyframes + heading dark styling

--- curious/src/pages/search.tsx
+++ curious/src/pages/search.tsx
@@ -54,8 +54,8 @@ export default function Home() {
 
         <main className="bg-white dark:bg-anthracite flex w-full flex-col items-center justify-center text-center">
           <div className="w-full h-full flex flex-col items-center justify-start px-4 sm:px-10 pt-4 pb-8 sm:py-10 overflow-scroll">
-            {(resources || loading) && <div className="max-w-xl w-full mt-2">
-              <h2 className="sm:text-1xl text-4xl font-bold max-w-[708px] mb-5 text-slate-900">
+            {(resources || loading) && <div className="max-w-xl w-full mt-10">
+              <h2 className="sm:text-1xl text-4xl font-bold max-w-[708px] mb-10 text-dark dark:text-white">
                 Understand the subject 💡
               </h2>
             </div>}
@@ -107,7 +107,7 @@ export default function Home() {
             })}
 
             {(resources || loading) && <div className="max-w-xl w-full mt-10">
-              <h2 className="sm:text-1xl text-4xl font-bold max-w-[708px] mb-10 text-slate-900">
+              <h2 className="sm:text-1xl text-4xl font-bold max-w-[708px] mb-10 text-dark dark:text-white">
                 Want more? 🚀
               </h2>
             </div>
