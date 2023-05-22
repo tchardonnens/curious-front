79db84679215a89a70310aa13b9bee81c4d36671
Thomas Chardo <chardonnens.thomas@protonmail.com>
2023-05-22 15:57:15 +0200

    (fix): fix header

--- curious/src/components/header.tsx
+++ curious/src/components/header.tsx
@@ -17,7 +17,7 @@ export default function Header({ setIsSidebarOpen }: HeaderProps) {
   let sidebarBtn;
   const indicatorRef = useRef<HTMLDivElement | null>(null);
   const searchRef = useRef<HTMLAnchorElement | null>(null);
-  const feedRef = useRef<HTMLAnchorElement | null>(null);  
+  const feedRef = useRef<HTMLAnchorElement | null>(null);
 
   useEffect(() => {
     const path = router.pathname;
@@ -35,7 +35,7 @@ export default function Header({ setIsSidebarOpen }: HeaderProps) {
       }
     }
   }, [router.pathname]);
-  
+
 
   if (router.pathname === '/feed' || router.pathname === '/search') {
     headerContent = (
@@ -54,9 +54,7 @@ export default function Header({ setIsSidebarOpen }: HeaderProps) {
         </Link>
       </>
     );
-  } else if (router.pathname === '/signup' || router.pathname === '/signin' || router.pathname === '/profile') {
-    headerContent = null;
-  } else {
+  } else if (router.pathname === '/') {
     headerContent = (
       <div className='flex flex-row gap-4'>
         <Link href="/signup" title='Sign Up'>
