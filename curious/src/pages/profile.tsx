3794ef31cffff98cadb847e693e83a206b0bd4a6
Bora <borabalos9@gmail.com>
2023-05-22 15:31:09 +0200

    Clean code for profile

--- curious/src/pages/profile.tsx
+++ curious/src/pages/profile.tsx
@@ -37,73 +37,54 @@ const Profile: React.FC<ProfileProps> = ({
 
       <div className="flex flex-row w-full h-full">
 
-        <Sidebar history={[]}></Sidebar>
+        <Sidebar isSidebarOpen={false} history={[]} ></Sidebar>
 
 
 
         <main className="bg-white dark:bg-anthracite p-10 flex w-full flex-col overflow-hidden">
+          <div className="flex items-center relative">
 
-          <div>
-
-            <div className="flex items-center relative">
-
-              <div className="w-full h-40">
-                <Image src="sf-dream.png" alt="Avatar" className="w-full h-full object-cover rounded-t-xl" />
-              </div>
-
-              <div className="w-24 h-24 rounded-full overflow-hidden absolute bottom-0 left-6 translate-y-12">
-                <Image src="PP-test.png" alt="Avatar" className="w-full h-full object-cover" />
-              </div>
-
+            <div className="w-full h-40">
+              <Image src="sf-dream.png" alt="Avatar" className="w-full h-full object-cover rounded-t-xl" />
             </div>
 
-            <div className="flex flex-col space-y-4 w-1/2 mt-10 px-4">
-              <div className="flex gap-4 items-center pt-6">
-                <div className="font-semibold text-lg">
-                  Name{name}
-                </div>
-
-                <div className="text-neutral-600">
-                  @username{username}
-                </div>
-              </div>
-
-
-              <div className="text-gray-600">
-                Bio ...... ....... ........ ....... ........ .......... ........... ......... ............ ............ .......... ......... ........{bio}
-              </div>
-
-              <div className="flex flex-row space-x-20">
-
-                <div className="flex flex-row space-x-4">
-
-                  <div>
-                    <span className="text-gray-600">Followers</span>
-                  </div>
-
-                  <div>
-                    <span className="font-semibold">180{followers}</span>
-                  </div>
-
-                </div>
-
-                <div className="flex flex-row space-x-4">
-
-                  <div>
-                    <span className="text-gray-600">Following</span>
-                  </div>
-
-                  <div>
-                    <span className="font-semibold">200{following}</span>
-                  </div>
-
-                </div>
-
-              </div>
-
+            <div className="w-24 h-24 rounded-full overflow-hidden absolute bottom-0 left-6 translate-y-12">
+              <Image src="PP-test.png" alt="Avatar" className="w-full h-full object-cover" />
             </div>
 
           </div>
+
+          <div className="flex flex-col space-y-4 w-1/2 mt-10 px-4">
+
+            <div className="flex flex-row gap-4 items-center pt-6">
+              <div className="font-semibold text-lg text-black dark:text-white">
+                Name{name}
+              </div>
+
+              <div className="text-darkerGrey dark:text-darkGrey">
+                @username{username}
+              </div>
+            </div>
+
+            <div className="text-darkerGrey dark:text-darkGrey">
+              Bio ...... ....... ........ ....... ........ .......... ........... ......... ............ ............ .......... ......... ........{bio}
+            </div>
+
+
+            <div className="flex flex-row gap-6">
+              <div className="text-darkerGrey dark:text-darkGrey">
+                <span className="font-semibold text-black dark:text-white mr-2">200{following}</span>
+                Following
+              </div>
+
+              <div className="text-darkerGrey dark:text-darkGrey">
+                <span className="font-semibold text-black dark:text-white mr-2">180{followers}</span>
+                Followers
+              </div>
+            </div>
+
+          </div>
+
         </main>
       </div>
     </>
