84728f947c4039423f621b4772b83e9e6e5facaa
Bora <borabalos9@gmail.com>
2023-06-12 16:12:44 +0200

    Cards for Profile page Posts

--- curious/src/components/post.tsx
+++ curious/src/components/post.tsx
@@ -3,7 +3,7 @@ import Card from '@/components/card';
 import Image from 'next/image';
 import ProfilePic from './profilePic';
 
-type PostProps = {
+interface PostProps {
   /*userPP : string;
   userURL : string;
   username : string;
@@ -13,18 +13,27 @@ type PostProps = {
   cardDescription: string;
   cardImage: string;
   cardURL: string;
-  time : Date;
-*/};
+  time : Date;*/
+  removeUserInfo?: string;
+  removeDatePadding?: string;
+  promptDetailPadding?: string;
+  removePostMargin?: string;
+};
 
-export default function Post(props: PostProps) {
+const Post: React.FC<PostProps> = ({
+  removeUserInfo,
+  removeDatePadding,
+  promptDetailPadding,
+  removePostMargin
+}) => {
   const [username, setUsername] = useState("John Doe");
   const [prompt, setPrompt] = useState("How to make a website");
 
   return (
-    <div className='w-full mb-8'>
+    <div className={`w-full mb-8 ${removePostMargin}`}>
       <div className='flex flex-col justify-start w-full rounded-lg md:p-6 mb-3'>
 
-        <div className='flex flex-col items-center justify-center gap-2'>
+        <div className={`flex flex-col items-center justify-center gap-2 ${removeUserInfo}`}>
           {/* <div className="w-10 h-10 rounded-full overflow-hidden border-2 border-darkGrey dark:border-white">
             <Image src="/PP-test.png" alt="Avatar" width={200} height={200} className="w-full h-full object-cover" />
           </div> */}
@@ -32,10 +41,10 @@ export default function Post(props: PostProps) {
           <h2 className="text-2xl text-black dark:text-white">{username}</h2>
         </div>
 
-        <div className='text-neutral-600 dark:text-neutral-400 p-4 mb-3'>
+        <div className={`text-neutral-600 dark:text-neutral-400 p-4 mb-3 ${promptDetailPadding}`}>
           <h3>Searched for prompt: <span className='font-medium text-black dark:text-white'>{prompt}</span></h3>
           
-          <div className='text-neutral-600 dark:text-neutral-400 p-4'>
+          <div className={`text-neutral-600 dark:text-neutral-400 p-4 ${removeDatePadding}`}>
             Monday 12:00 PM
           </div>
         </div>
@@ -50,3 +59,5 @@ export default function Post(props: PostProps) {
     </div>
   );
 }
+
+export default Post;
